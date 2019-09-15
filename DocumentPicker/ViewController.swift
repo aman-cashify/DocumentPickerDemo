2ed386f546b8319a81400d97c315478f39e9b99f
NK Jagne <nkjagne@NKs-MacBook-Pro.local>
2019-09-15 23:56:58 +0530

    first commit

--- DocumentPicker/ViewController.swift
+++ DocumentPicker/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIDocumentPickerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let documentPicker = UIDocumentPickerViewController(documentTypes: [String(kUTTypeText),String(kUTTypeContent),String(kUTTypeItem),String(kUTTypeData)], in: .import)
+        //Call Delegate
+        documentPicker.delegate = self
+        self.present(documentPicker, animated: true)
+        
+    }
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        print(urls)
     }
 
 
